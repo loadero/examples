27654ef86a4657158da12ac1281ec204b2b848e6
Alekss Litvinovs <alekss.litvinovs@testdevlab.com>
2020-12-10 16:09:54 +0200

    Use 2 setValue() calls instead of 1 with array of inputs

--- javascript/tests/nw_tutorial/part1/duckduckgo_test_loadero.js
+++ javascript/tests/nw_tutorial/part1/duckduckgo_test_loadero.js
@@ -4,7 +4,8 @@ client => {
     client
         .url('https://duckduckgo.com/')
         .waitForElementVisible('#content_homepage', 10 * 1000)
-        .setValue('#search_form_input_homepage', ['Nightwatch.js', client.Keys.ENTER])
+        .setValue('#search_form_input_homepage', 'Nightwatch.js')
+        .setValue('#search_form_input_homepage', client.Keys.ENTER)
         .assert.containsText('#r1-0 .result__title', 'Nightwatch.js')
         .click('#r1-0 .result__title')
         .takeScreenshot('NightwatchJS.png')
--- javascript/tests/nw_tutorial/part1/duckduckgo_test_local.js
+++ javascript/tests/nw_tutorial/part1/duckduckgo_test_local.js
@@ -5,7 +5,8 @@ module.exports = {
         client
             .url('https://duckduckgo.com/')
             .waitForElementVisible('#content_homepage', 10 * 1000)
-            .setValue('#search_form_input_homepage', ['Nightwatch.js', client.Keys.ENTER])
+            .setValue('#search_form_input_homepage', 'Nightwatch.js')
+            .setValue('#search_form_input_homepage', client.Keys.ENTER)
             .assert.containsText('#r1-0 .result__title', 'Nightwatch.js')
             .click('#r1-0 .result__title')
             .saveScreenshot('NightwatchJS.png')
