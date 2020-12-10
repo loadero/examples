a941a8c57676bfb37b39d40bbcd93a58401c3be3
Alekss Litvinovs <alekss.litvinovs@testdevlab.com>
2020-12-10 15:02:12 +0200

    Use DuckDuckGo instead of Google in scripts

---
+++ javascript/tests/nw_tutorial/part1/duckduckgo_test_loadero.js
@@ -0,0 +1,12 @@
+client => {
+    // Original article https://blog.loadero.com/
+
+    client
+        .url('https://duckduckgo.com/')
+        .waitForElementVisible('#content_homepage', 10 * 1000)
+        .setValue('#search_form_input_homepage', ['Nightwatch.js', client.Keys.ENTER])
+        .assert.containsText('#r1-0 .result__title', 'Nightwatch.js')
+        .click('#r1-0 .result__title')
+        .takeScreenshot('NightwatchJS.png')
+        .pause(5 * 1000);
+}
---
+++ javascript/tests/nw_tutorial/part1/duckduckgo_test_local.js
@@ -0,0 +1,14 @@
+// Original article https://blog.loadero.com/
+
+module.exports = {
+    test: client => {
+        client
+            .url('https://duckduckgo.com/')
+            .waitForElementVisible('#content_homepage', 10 * 1000)
+            .setValue('#search_form_input_homepage', ['Nightwatch.js', client.Keys.ENTER])
+            .assert.containsText('#r1-0 .result__title', 'Nightwatch.js')
+            .click('#r1-0 .result__title')
+            .saveScreenshot('NightwatchJS.png')
+            .pause(5 * 1000);
+    }
+};
