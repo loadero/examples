fb87f1d293a1575ed9aed1bcc2a68db6ea33b6ef
Alekss Litvinovs <alekss.litvinovs@loadero.com>
2021-06-21 10:40:53 +0300

    Add local and Loadero scripts from Nightwatch.js tutorial part 3

---
+++ javascript/tests/nw_tutorial/part3/command_queue_local.js
@@ -0,0 +1,21 @@
+// Original article <url>
+
+module.exports = {
+    test: client => {
+        let text;
+
+        client
+            .url('https://duckduckgo.com/')
+            .perform(() => {
+                text = 'first'; // assigns 'first' to text
+            })
+            .setValue('#search_form_input_homepage', text)
+            .getValue('#search_form_input_homepage', ({ value }) => console.log(value))
+            .clearValue('#search_form_input_homepage') // clears input field
+            .perform(() => client.setValue('#search_form_input_homepage', text))
+            .getValue('#search_form_input_homepage', ({ value }) => console.log(value));
+
+        console.log('second');
+    }
+};
+
