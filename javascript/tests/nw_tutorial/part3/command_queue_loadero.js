812c164107608291c641d0bd80d2bdb8d7de0891
Alekss Litvinovs <alekss.litvinovs@loadero.com>
2021-06-22 10:05:55 +0300

    Add link to the article

--- javascript/tests/nw_tutorial/part3/command_queue_loadero.js
+++ javascript/tests/nw_tutorial/part3/command_queue_loadero.js
@@ -1,4 +1,4 @@
-// Original article <url>
+// Original article https://blog.loadero.com/2021/06/22/a-beginners-guide-to-test-automation-with-javascriptnightwatch-js-part-3
 
 client => {
     let text;
--- javascript/tests/nw_tutorial/part3/command_queue_local.js
+++ javascript/tests/nw_tutorial/part3/command_queue_local.js
@@ -1,4 +1,4 @@
-// Original article <url>
+// Original article https://blog.loadero.com/2021/06/22/a-beginners-guide-to-test-automation-with-javascriptnightwatch-js-part-3
 
 module.exports = {
     test: client => {
@@ -18,4 +18,3 @@ module.exports = {
         console.log('second');
     }
 };
-
