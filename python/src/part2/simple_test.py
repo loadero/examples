da0259774c9a03833ecd606f1e6b8b85f857bd28
rudolfs.zvejs <rudolfs.zvejs@loadero.com>
2022-05-20 10:05:47 +0300

    py-local-setup: Re-format

--- python/src/part2/simple_test.py
+++ python/src/part2/simple_test.py
@@ -3,7 +3,12 @@
 import time
 from testui.support.appium_driver import NewDriver
 
-driver = NewDriver().set_logger().set_browser("chrome").set_selenium_driver()
+driver = (
+    NewDriver()
+    .set_logger()
+    .set_browser("chrome")
+    .set_selenium_driver()
+)
 
 driver.navigate_to("https://loadero.com/")
 time.sleep(10)
