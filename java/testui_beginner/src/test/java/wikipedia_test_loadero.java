67d139277e6056dffaf17b82445b5e1411da14c6
elizlac <112175670+elizlac@users.noreply.github.com>
2022-08-30 17:12:28 +0300

    Apply suggestions from code review
    Co-authored-by: Alekss Litvinovs <52907932+theRealAlpaca@users.noreply.github.com>

--- java/testui_beginner/src/test/java/wikipedia_test_loadero.java
+++ java/testui_beginner/src/test/java/wikipedia_test_loadero.java
@@ -1,28 +1,28 @@
 public void testUIWithLoadero(){
-    //HTML elements that will be used
+    // HTML elements that will be used
     UIElement searchInput=E(byCssSelector("input#searchInput"));
     UIElement searchButton=E(byId("searchButton"));
     UIElement todaysArticle=E(byCssSelector("#mp-tfa p"));
     UIElement pageContainer=E(byId("content"));
 
-    //open url and wait until page container is visible
+    // open url and wait until page container is visible
     open("https://en.wikipedia.org/wiki/Main_Page");
     pageContainer.waitFor(10).untilIsVisible();
 
-    //get text of today's article
+    // get text of today's article
     String text=todaysArticle.getText();
-    System.out.printf("Todays featured article is:\n %s",text);
+    System.out.printf("Todays featured article is:\n %s\n",text);
 
     // input text in search input field
     searchInput.waitFor(10).untilIsVisible().sendKeys("Selenium Webdriver");
     searchButton.click();
     // alternative: searchInput.sendKeys(Keys.ENTER);
 
-    //take a screenshot
+    // take a screenshot
     pageContainer.waitFor(10).untilIsVisible().saveScreenshot("screenshot.png");
 
-    //save all search results titles in a `collection`
+    // save all search results titles in a `collection`
     UICollection collection=EE(byCssSelector(".mw-search-result-heading"));
     String firstResult=collection.first().getText();
-    System.out.printf("First result is:\n%s",firstResult);
-    }
+    System.out.printf("First result is:\n%s\n",firstResult);
+}
