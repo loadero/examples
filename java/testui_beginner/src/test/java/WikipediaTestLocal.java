a2d9ffbbc3004587e511f44eed3c9d79cd212228
elizlac <112175670+elizlac@users.noreply.github.com>
2022-08-31 11:55:17 +0300

    Add script examples for TestUI beginner blog post (#13)
    * Create wikipedia_test_local.java
    
    * Add examples
    
    * Update java/testui_beginner/wikipedia_test_loadero.java
    
    Co-authored-by: Alekss Litvinovs <52907932+theRealAlpaca@users.noreply.github.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: Alekss Litvinovs <52907932+theRealAlpaca@users.noreply.github.com>
    
    * conform with Java general project structure
    
    * Apply suggestions from code review
    
    Co-authored-by: Alekss Litvinovs <52907932+theRealAlpaca@users.noreply.github.com>
    
    * fix formatting
    
    * Define version variables in properties and use them for deps and plugins.
    
    * Remove second UICollection example
    
    * Apply suggestions from code review
    
    Co-authored-by: Alekss Litvinovs <52907932+theRealAlpaca@users.noreply.github.com>
    
    * Add blog link and tesui dependency
    
    * Fix class name
    
    Co-authored-by: Alekss Litvinovs <52907932+theRealAlpaca@users.noreply.github.com>

---
+++ java/testui_beginner/src/test/java/WikipediaTestLocal.java
@@ -0,0 +1,43 @@
+import static testUI.UIOpen.open;
+import static testUI.Utils.By.byCssSelector;
+import static testUI.Utils.By.byId;
+import static testUI.elements.TestUI.E;
+import static testUI.collections.TestUI.EE;
+
+import testUI.collections.UICollection;
+import testUI.elements.UIElement;
+import org.junit.jupiter.api.Test;
+
+public class WikipediaTestLocal {
+    @Test
+    public void test() {
+        // Original article https://blog.loadero.com/2022/08/30/beginners-guide-to-test-automation-with-java-testui/
+
+        // HTML elements that will be used
+        UIElement searchInput = E(byCssSelector("input#searchInput"));
+        UIElement searchButton = E(byId("searchButton"));
+        UIElement todaysArticle = E(byCssSelector("#mp-tfa p"));
+        UIElement pageContainer = E(byId("content"));
+
+        // open url and wait until page container is visible
+        open("https://en.wikipedia.org/wiki/Main_Page");
+        pageContainer.waitFor(10).untilIsVisible();
+
+        // get text of today's article
+        String text = todaysArticle.getText();
+        System.out.printf("Todays featured article is:\n%s\n", text);
+
+        // input text in search input field
+        searchInput.waitFor(10).untilIsVisible().sendKeys("Selenium Webdriver");
+        searchButton.click();
+        // alternative: searchInput.sendKeys(Keys.ENTER);
+
+        // take a screenshot
+        pageContainer.waitFor(10).untilIsVisible().saveScreenshot("screenshot.png");
+
+        // save all search results titles in a `collection`
+        UICollection collection = EE(byCssSelector(".mw-search-result-heading"));
+        String firstResult = collection.first().getText();
+        System.out.printf("First result is:\n%s\n", firstResult);
+    }
+}
